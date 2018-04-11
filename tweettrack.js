39d9b6a9079299c623dc01fd8bf26fe0b35569d4
Tim Spear <tim@tspear.com>
2018-04-11 23:21:52 +0100

    init

---
+++ tweettrack.js
@@ -0,0 +1,11 @@
+const songlist = [
+  {songTitle:"Decision", artist:"Secret Nuclear", service:"Spotify", url:"https://bit.ly/savewhi002"},
+  {songTitle:"Mid-century Paranoia", artist:"Secret Nuclear", service:"Spotify", url:"https://bit.ly/savewhi002"},
+  {songTitle:"Vega", artist:"Secret Nuclear", service:"Spotify", url:"https://bit.ly/savewhi003"}
+]
+
+const currentTrack = songlist[1];
+
+let tweet = `${}`;
+
+console.log(tweet);
